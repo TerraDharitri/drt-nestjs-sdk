8a5eb47aabcabf987962b1d3005f7d28af7215a8
kasiaurangabad <kasi.aurangabad@gmail.com>
2025-05-23 16:33:32 +0530

    v0.0.3

--- packages/common/src/utils/url.utils.ts
+++ packages/common/src/utils/url.utils.ts
@@ -1,8 +1,16 @@
 export class UrlUtils {
-  static isLocalhost(url: string): boolean {
+  static isLocalhost(url: string, shouldBeHttps: boolean = true): boolean {
     try {
       const requestUrl = new URL(url);
-      return requestUrl.hostname === 'localhost';
+      if (requestUrl.hostname !== 'localhost') {
+        return false;
+      }
+
+      if (shouldBeHttps && requestUrl.protocol !== 'https:') {
+        return false;
+      }
+
+      return true;
     } catch {
       return false;
     }
